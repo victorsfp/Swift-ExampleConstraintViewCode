5d4add728bd30b3b9b52b70c2da79adf31ef2268
Victor Feitosa <victor.s.feitosa2@hotmail.com>
2022-01-18 16:42:18 -0300

    Introduction Constraint and frames

---
+++ ExampleConstraintViewCode/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  ExampleConstraintViewCode
+//
+//  Created by Victor Feitosa on 18/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let viewAzul = UIView()
+    let viewVermelha = UIView()
+    let label = UILabel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        viewAzul.translatesAutoresizingMaskIntoConstraints = false
+        viewAzul.backgroundColor = .blue
+        
+        viewVermelha.translatesAutoresizingMaskIntoConstraints = false
+        viewVermelha.backgroundColor = .red
+        
+        //viewAzul.frame = CGRect(x: 10, y: 100, width: 370, height: 100)
+        //viewVermelha.frame = CGRect(x: 10, y: viewAzul.frame.maxY + 10, width: 150, height: 100)
+        
+        //viewAzul.frame = CGRect(x: 10, y: 100, width: UIScreen.main.bounds.width - 20, height: 100)
+        //viewVermelha.frame = CGRect(x: 10, y: viewAzul.frame.maxY + 10, width: UIScreen.main.bounds.width - 20, height: 100)
+        
+        //print(UIScreen.main.bounds) //TAMANHO DA TELA
+        
+        view.addSubview(viewAzul)
+        view.addSubview(viewVermelha)
+        view.backgroundColor = .white
+        
+        NSLayoutConstraint.activate([
+            viewAzul.topAnchor.constraint(equalTo: view.topAnchor),
+            viewAzul.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            viewAzul.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            viewAzul.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            viewVermelha.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            viewVermelha.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            viewVermelha.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            viewVermelha.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
+//        let constraints = [
+//            viewAzul.topAnchor.constraint(equalTo: view.topAnchor),
+//            viewAzul.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//            viewAzul.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+//            viewAzul.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+//
+//            viewVermelha.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+//            viewVermelha.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+//            viewVermelha.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+//            viewVermelha.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+//        ]
+//        constraints.forEach { item in
+//            item.isActive = true
+//        }
+        
+        
+    }
+
+
+}
+
